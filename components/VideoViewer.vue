436c8471089a1aaa034a91449bc46980fb7b4586
Anne-Victoria <Anne-Victoria@users.noreply.github.com>
2020-10-27 16:04:05 +0100

    Format VideoViewer code

--- components/VideoViewer.vue
+++ components/VideoViewer.vue
@@ -1,30 +1,32 @@
 <template>
-<div>
-  <div class="video-js-container">
-    <video class="video-js vjs-big-play-centered"
-      ref="videoRef"
-      id="videojs-container"
-      controls
-      NOautoplay
-      preload="auto"
-      poster="/stream-images/stream_poster.jpg"
-      data-setup="{}">
-    <source :src="livestreamUrl" type="application/x-mpegURL" />
-      <p class="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a
-        web browser that
-        <a href="https://videojs.com/html5-video-support/" target="_blank"
-          >supports HTML5 video</a
-        >
-      </p>
-    </video>
-  </div>
+  <div>
+    <div class="video-js-container">
+      <video
+        class="video-js vjs-big-play-centered"
+        ref="videoRef"
+        id="videojs-container"
+        controls
+        NOautoplay
+        preload="auto"
+        poster="/stream-images/stream_poster.jpg"
+        data-setup="{}"
+      >
+        <source :src="livestreamUrl" type="application/x-mpegURL" />
+        <p class="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to
+          a web browser that
+          <a href="https://videojs.com/html5-video-support/" target="_blank"
+            >supports HTML5 video</a
+          >
+        </p>
+      </video>
+    </div>
   </div>
 </template>
 
 
 <script>
-import videojs from 'video.js'
+import videojs from 'video.js';
 export default {
   props: {
     livestreamUrl: String,
@@ -32,7 +34,7 @@ export default {
   data() {
     return {
       player: null,
-    }
+    };
   },
   mounted() {
     const videoObj = this.$refs.videoRef;
@@ -40,8 +42,8 @@ export default {
   },
   beforeDestroy() {
     this.player.dispose();
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
@@ -50,19 +52,18 @@ $big-play-button--line-height: 2.25em;
 $big-play-button--width: 4.5em;
 $primary-foreground-color: #fff;
 $primary-background-color: $color-darkblue;
-@import "video.js/src/css/video-js.scss";
-
+@import 'video.js/src/css/video-js.scss';
 
 .video-js-container {
-    padding-top: 56.25%; /* 16:9 */
-    width: 100%;
-    position: relative;
+  padding-top: 56.25%; /* 16:9 */
+  width: 100%;
+  position: relative;
 }
 .video-js-container .video-js {
-    height: 100% !important;
-    width: 100% !important;
-    position: absolute;
-    top: 0;
-    left: 0;
+  height: 100% !important;
+  width: 100% !important;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
