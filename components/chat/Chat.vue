c2a0b7a711580bded40c2e239a2304186f86e1c4
Anne-Victoria <Anne-Victoria@users.noreply.github.com>
2021-10-08 00:21:27 +0200

    Add links in chat section

---
+++ components/chat/Chat.vue
@@ -0,0 +1,41 @@
+<template>
+  <section :class="wrapperClasses">
+    <kiwi-chat v-if="hasConsentedToChat" />
+    <ChatConsent v-else @consent-to-chat="consentToChat()" />
+  </section>
+</template>
+
+<script>
+import KiwiChat from './KiwiChat.vue';
+export default {
+  components: { KiwiChat },
+  data() {
+    return {
+      hasConsentedToChat: false,
+    };
+  },
+  computed: {
+    wrapperClasses() {
+      const className = this.hasConsentedToChat
+        ? 'box fullHeight'
+        : 'box';
+      return className;
+    }
+  },
+  methods: {
+    consentToChat() {
+      this.hasConsentedToChat = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/scss/main.scss';
+
+@include widescreen {
+  .fullHeight {
+    height: 100%;
+  }
+}
+</style>
---
+++ components/chat/ChatConsent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3 class="title is-4">{{ $t('chat.title') }}</h3>
+    <div class="content">
+      <p>{{ $t('chat.intro') }}</p>
+      <ul class="content">
+        <li>{{ $t('chat.public') }}</li>
+        <i18n path="chat.privacyPolicyText" tag="li">
+          <a href="https://privacyweek.at/datenschutz/">{{
+            $t('chat.privacyPolicy')
+          }}</a>
+        </i18n>
+        <i18n path="chat.rulesText" tag="li">
+          <a
+            href="https://projekte.c3w.at/pw21online_public_nutzungsbedingungen"
+            >{{ $t('chat.rules') }}</a
+          >
+        </i18n>
+        <i18n path="chat.faqText" tag="li">
+          <a href="https://projekte.c3w.at/pw21online_faq_publikum">{{
+            $t('chat.faq')
+          }}</a>
+        </i18n>
+      </ul>
+    </div>
+    <button class="button is-dark is-rounded" @click="$emit('consent-to-chat')">
+      {{ $t('chat.accept') }}
+    </button>
+  </div>
+</template>
